scala/scala3

Erase phantom fields.
Erase all `val` and `var` of phantom type. `lazy val` are not transformed as their expansion is done in the backend.

Erasure:
* Assignments are replaced by their RHS
* Local `val` and `var` definitions are replaced by their RHS
* Fields are removed
* Getters return `null.asInstanceOf[dotty.runtime.ErasedPhantom]`

Tests for this commit where added in `Extra tests for Phantoms 2`, they where back ported as the semantics and runtime is not affected.

compiler/src/dotty/tools/dotc/Compiler.scala
@@ -94,6 +94,7 @@ class Compiler {
       List(new LambdaLift,          // Lifts out nested functions to class scope, storing free variables in environments
                                        // Note: in this mini-phase block scopes are incorrect. No phases that rely on scopes should be here
            new ElimStaticThis,      // Replace `this` references to static objects by global identifiers
+           new PhantomVals,         // Remove ValDefs of phantom type
            new Flatten,             // Lift all inner classes to package scope
            new RestoreScopes),      // Repair scopes rendered invalid by moving definitions in prior phases of the group
       List(new MoveStatics,         // Move static methods to companion classes

nicolasstucki/dotty

compiler/src/dotty/tools/dotc/Compiler.scala
@@ -91,6 +91,7 @@ class Compiler {
       List(new LambdaLift,          // Lifts out nested functions to class scope, storing free variables in environments
                                        // Note: in this mini-phase block scopes are incorrect. No phases that rely on scopes should be here
            new ElimStaticThis,      // Replace `this` references to static objects by global identifiers
+           new PhantomFields,       // Remove fields with phantom type
            new Flatten,             // Lift all inner classes to package scope
            new RestoreScopes),      // Repair scopes rendered invalid by moving definitions in prior phases of the group
       List(new MoveStatics,         // Move static methods to companion classes