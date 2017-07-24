6492d89d638221534e7148953e241240463dc0d6
Nicolas Stucki <nicolas.stucki@gmail.com>
2017-07-24 14:18:25 +0200

    Add link tests

---
+++ tests/link/on-custom-lib/map2.scala
@@ -0,0 +1,8 @@
+
+object Test {
+  def main(args: Array[String]): Unit = {
+    // FIMXE
+//    val map2 = new Map2[Int] {}
+//    println(map2.foo)
+  }
+}
---
+++ tests/link/strawman/nil.scala
@@ -0,0 +1,6 @@
+object Test {
+  def main(args: Array[String]): Unit = {
+    // FIXME: java.lang.AssertionError: assertion failed: position not set for DefDef(head,List()
+    // strawman.collection.immutable.Nil
+  }
+}
